1d170769647e50173b11669561a84adbc67df758
Bruno Calasans <83395524+Bruno-Calasans@users.noreply.github.com>
2022-10-30 19:13:12 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import type { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Home Page")
+})
+
+app.listen(8000, (): void => {
+	console.log("Server running!")
+})
+
+
+
